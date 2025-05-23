564610b4c6d74e9486e4b5a4d6dc34efeed485c9
Allanhtyz <allanjoao69@gmail.com>
2025-05-23 17:08:18 -0300

    Add files via upload

---
+++ exerc1.cs
@@ -0,0 +1,17 @@
+﻿
+ namespace projeto;
+
+ public class exerc1(){
+
+    public static void Main(){
+        int idade = Convert.ToInt32(Console.ReadLine());
+        if (idade >= 18)
+         {
+            Console.WriteLine("Você é maior de idade.");
+        }
+        else {
+           Console.WriteLine("Você é menor de idade.");
+
+        }
+    }
+ }
---
+++ exerc2.cs
@@ -0,0 +1,29 @@
+namespace projeto;
+
+ class exerc2
+{
+    static void Main()
+    {
+        double nota1, nota2, nota3, media;
+
+        Console.Write("Digite a primeira nota: ");
+        nota1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite a segunda nota: ");
+        nota2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite a terceira nota: ");
+        nota3 = Convert.ToDouble(Console.ReadLine());
+
+        media = (nota1 + nota2 + nota3) / 3;
+
+        if (media >= 7.0)
+        {
+            Console.WriteLine($"Média: {media:F2} - Aluno Aprovado!");
+        }
+        else
+        {
+            Console.WriteLine($"Média: {media:F2} - Aluno Reprovado.");
+        }
+    }
+}
